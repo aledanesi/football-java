d9c20e1cd1ef96f9ef0f7b398f8c28c6fb16b71e
danesiale@gmail.com <danesiale@gmail.com@966f9feb-cae3-6a02-026f-4ad8e5c7e621>
2012-06-21 12:36:31 +0000

    modifica

--- src/com/progetto/dao/hibernate/TeamDaoImpl.java
+++ src/com/progetto/dao/hibernate/TeamDaoImpl.java
@@ -24,104 +24,103 @@
 package com.progetto.dao.hibernate;
 
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+
 import org.hibernate.validator.InvalidStateException;
 import org.hibernate.validator.InvalidValue;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import com.googlecode.s2hibernate.struts2.plugin.annotations.SessionTarget;
-import com.googlecode.s2hibernate.struts2.plugin.annotations.TransactionTarget;
+
 import com.progetto.dao.TeamDao;
-import com.progetto.domain.Division;
 import com.progetto.domain.Team;
 
 /**
  * @author C_ICTDNS
- * 
+ *
  */
 @Repository("teamDAO")
-public class TeamDaoImpl extends HibernateDaoSupport implements TeamDao {
-
-
-	/***************************************************************************
-	 * 
-	 * METHODS
-	 * 
-	 **************************************************************************/
-
+public class TeamDaoImpl extends HibernateDaoSupport implements TeamDao
+{
+	
+	
+	/*************************************************
+	  
+	  METHODS
+	  
+	 **************************************************/	
+	
 	/**
 	 * Method to save team
 	 * 
-	 * @param team
-	 *            the team to save
+	 * @param team the team to save
 	 */
 	@Override
-	public void saveOrUpdateTeam(Team team) {
- 		getHibernateTemplate().saveOrUpdate(team);
+	public void saveOrUpdateTeam(Team team) 
+	{
+		try 
+		{
+			getHibernateTemplate().saveOrUpdate(team);	
+		}
+		catch(InvalidStateException e)
+		{
+			 for (InvalidValue invalidValue : e.getInvalidValues()) 
+			 {         
+				 System.out.println("Instance of bean class: " + invalidValue.getBeanClass().getSimpleName() + " has an invalid property: " + invalidValue.getPropertyName() + " with message: " + invalidValue.getMessage());     
+			 } 			
+		}
+		
+		
 	}
-
+	
 	/**
 	 * Method to list teams
 	 * 
 	 * @return the teams found
-	 */
+	 */	
 	@SuppressWarnings("unchecked")
-	@Override
-	public List<Team> listTeams() {
-		List<Team> lista = getHibernateTemplate().find(
-				"from Team t order by t.name ");
+	@Override	
+	public List<Team> listTeams() 
+	{
+		List<Team> lista = getHibernateTemplate().find("from Team t order by t.name ");
 		return lista;
 	}
-
+	
 	/**
-	 * Method to get a team list by a division
+	 * Method to get a team list by a division 
 	 * 
-	 * @param divisionId
-	 *            the division id
+	 * @param divisionId the division id
 	 * @return the teams found
-	 */
+	 */	
 	@SuppressWarnings("unchecked")
-	@Override
-	public List<Team> listTeamsByDivision(Long divisionId) {
-		return getHibernateTemplate().findByNamedParam(
-				"from Team t where division_id = :divisionId order by t.name",
-				"divisionId", divisionId);
-	}
-
-	/**
-	 * Method to list divisions
-	 * 
-	 * @return the divisions found
-	 */
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Division> listDivisions() {
-		return getHibernateTemplate().find("from Division d order by d.id");
-	}
-
+	@Override	
+	public List<Team> listTeamsByDivision(Long divisionId) 
+	{
+		return getHibernateTemplate().findByNamedParam("from Team t where division_id = :divisionId order by t.name", "divisionId", divisionId);
+	}	
+	
 	/**
 	 * Method to get team by id
 	 * 
-	 * @param teamId
-	 *            the team id
+	 * @param teamId the team id
 	 * @return the team found
 	 */
 	@Override
-	public Team getTeamById(Long teamId) {
-		return (Team) getHibernateTemplate().get(Team.class, teamId);
+	public Team getTeamById(Long teamId) 
+	{
+		return (Team)getHibernateTemplate().get(Team.class, teamId);
 	}
-
+	
 	/**
 	 * Method to delete a team
 	 * 
-	 * @param teamId
-	 *            the team id
-	 */
+	 * @param teamId the team id
+	 */		
 	@Override
-	public void deleteTeam(Long teamId) {
-		Team team = (Team) getHibernateTemplate().get(Team.class, teamId);
+	@Transactional
+	public void deleteTeam(Long teamId)
+	{
+		Team team = (Team)getHibernateTemplate().get(Team.class, teamId);
 		getHibernateTemplate().delete(team);
-	}
+	}	
+
 }
