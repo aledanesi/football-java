819a07c2164f749574b1446e8b80f3f570de813a
danesiale@gmail.com <danesiale@gmail.com@966f9feb-cae3-6a02-026f-4ad8e5c7e621>
2012-06-21 12:26:55 +0000

    modifica

--- src/com/progetto/action/TeamAction.java
+++ src/com/progetto/action/TeamAction.java
@@ -2,10 +2,12 @@ package com.progetto.action;
 
 import java.io.IOException;
 import java.util.List;
+
 import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
@@ -14,6 +16,7 @@ import org.apache.struts2.interceptor.ServletRequestAware;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.dao.DataIntegrityViolationException;
+
 import com.googlecode.s2hibernate.struts2.plugin.annotations.SessionTarget;
 import com.googlecode.s2hibernate.struts2.plugin.annotations.TransactionTarget;
 import com.opensymphony.xwork2.ActionContext;
@@ -24,219 +27,245 @@ import com.progetto.domain.Division;
 import com.progetto.domain.Nation;
 import com.progetto.domain.Player;
 import com.progetto.domain.Team;
+import com.progetto.manager.DivisionManager;
 import com.progetto.manager.NationManager;
 import com.progetto.manager.PlayerManager;
 import com.progetto.manager.TeamManager;
 import com.progetto.util.ProjectUtil;
 
-public class TeamAction extends FileUploadAction implements
-		ServletRequestAware, ModelDriven<Team> {
+public class TeamAction extends FileUploadAction implements ServletRequestAware, ModelDriven<Team>
+{
 	private static final long serialVersionUID = 1L;
-
-	@SessionTarget
+	
+	@SessionTarget 
 	Session session;
-
-	@TransactionTarget
-	Transaction transaction;
+	
+	@TransactionTarget   
+	Transaction transaction; 
 
 	private Team team;
-
 	private Player player;
-
+	
 	private TeamManager teamManager;
-
 	private PlayerManager playerManager;
-
 	private NationManager nationManager;
-
+	private DivisionManager divisionManager;
+	
 	private List<Team> teamList;
-
 	private List<Player> playerList;
-
 	private List<Division> divisionList;
-
 	private List<Nation> nationList;
-
+	
 	private ServletRequest servletRequest;
-
+	
 	private static final Long DEFAULT_DIVISION = 1L;
-
-	public TeamAction() {
-		teamManager = (TeamManager) SpringHelper.getBean("teamManager");
-		playerManager = (PlayerManager) SpringHelper.getBean("playerManager");
-		nationManager = (NationManager) SpringHelper.getBean("nationManager");
-
+	
+	public TeamAction()
+	{
+		teamManager = (TeamManager)SpringHelper.getBean("teamManager");
+		playerManager = (PlayerManager)SpringHelper.getBean("playerManager");
+		nationManager = (NationManager)SpringHelper.getBean("nationManager");
+		divisionManager = (DivisionManager)SpringHelper.getBean("divisionManager");
+		
 		team = new Team();
 		nationList = nationManager.listNations();
-		teamList = teamManager.listTeams();
-		divisionList = teamManager.listDivisions();
+		teamList = teamManager.getTeams();
+		divisionList = divisionManager.listDivisions();
 	}
-
+	
 	@Override
-	public Team getModel() {
+	public Team getModel() 
+	{
 		return team;
 	}
-
-	/*
-	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+	
+	/* _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
 	 * 
-	 * METODO VALIDATE
+	 *                             METODO VALIDATE
 	 * 
 	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
-	 */
-
-	public void validate() {
-		if (team.getName() != null && StringUtils.isEmpty(team.getName())) {
-			addActionError("Il nome è obbligatorio.");
+	 * */	
+	
+	public void validate()
+	{		
+		if ( team.getName() != null && StringUtils.isEmpty(team.getName()) )
+		{	
+			addActionError( "Il nome Ã¨ obbligatorio." );
 		}
-		if (team.getPosti() != null && StringUtils.isEmpty(team.getPosti())) {
-			addFieldError("team.posti", "I posti sono obbligatori.");
-		}
-	}
+		if ( team.getPosti() != null && StringUtils.isEmpty(team.getPosti()) )
+		{	
+			addFieldError( "team.posti", "I posti sono obbligatori." );
+		}				
+	}	
+	
+	
+	/* _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+	 * 
+	 *                             METODI DI ACTION
+	 * 
+	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+	 * */			
 
-	/*
-	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
-	 * 
-	 * METODI DI ACTION
-	 * 
-	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
-	 */
 	/**
 	 * To list a single user by Id.
-	 * 
 	 * @return String
 	 */
-	@Action(value = "/insertTeam", results = { @Result(name = "editTeam", location = "/content/editTeam.jsp") })
-	public String insert() {
-		divisionList = teamManager.listDivisions();
+	@Action( value   = "/insertTeam",
+			 results = { @Result(name  = "editTeam", location = "/content/editTeam.jsp") }
+	) 	
+	public String insert()
+	{
+		divisionList = divisionManager.listDivisions();
 		return ProjectUtil.EDIT_TEAM;
-	}
-
+	}	
+	
 	/**
 	 * To save or update user.
-	 * 
 	 * @return String
 	 */
-	@Action(value = "/saveOrUpdateTeam", results = {
-			@Result(name = "success", location = "/content/listTeam.jsp"),
-			@Result(name = "input", location = "/content/editTeam.jsp") })
-	public String saveOrUpdate() {
-		if (userImage != null) {
+	@Action( value   = "/saveOrUpdateTeam",
+			 results = { @Result(name  = "success", location = "/content/listTeam.jsp"),
+			             @Result(name  = "input",   location = "/content/editTeam.jsp")}
+	) 	
+	public String saveOrUpdate()
+	{	
+		if (userImage != null)
+		{
 			byte[] image = getFileBytes();
 			team.setImage(image);
-		} else if (team.getId() != null) {
+		}
+		else if(team.getId() != null)
+		{
 			team.setImage(teamManager.getTeamById(team.getId()).getImage());
 		}
-
+		
 		teamManager.saveOrUpdateTeam(team);
-
+		
 		return search();
 	}
-
+	
 	/**
 	 * To list all users.
-	 * 
 	 * @return String
 	 */
-	@Action(value = "/listTeam", results = { @Result(name = "success", location = "/content/listTeam.jsp") })
-	public String list() {
+	@Action( value   = "/listTeam",
+			 results = { @Result(name  = "success", location = "/content/listTeam.jsp") }
+	) 	
+	public String list()
+	{
 		Long divisionId = DEFAULT_DIVISION;
-
+		
 		teamList = teamManager.listTeamsByDivision(divisionId);
-		divisionList = teamManager.listDivisions();
-
+		divisionList = divisionManager.listDivisions();
+		
 		servletRequest.setAttribute("divisionId", divisionId);
-
+		
 		return SUCCESS;
-	}
-
+	}	
+	
 	/**
 	 * To list all users.
-	 * 
 	 * @return String
 	 */
-	@Action(value = "/searchTeam", results = { @Result(name = "success", location = "/content/listTeam.jsp") })
-	public String search() {
+	@Action( value   = "/searchTeam",
+			 results = { @Result(name  = "success", location = "/content/listTeam.jsp") }
+	) 	
+	public String search()
+	{
 		Long divisionId = DEFAULT_DIVISION;
-
-		if (team.getDivision().getId() != null) {
-			divisionId = team.getDivision().getId();
+		
+		if (team.getDivision().getId() != null)
+		{
+			divisionId = team.getDivision().getId();	
 		}
-
+		
 		teamList = teamManager.listTeamsByDivision(divisionId);
-		divisionList = teamManager.listDivisions();
-		servletRequest.setAttribute("divisionId", divisionId);
-		return SUCCESS;
-	}
+		divisionList = divisionManager.listDivisions();
 
+		servletRequest.setAttribute("divisionId", divisionId);
+
+		return SUCCESS;
+	}	
+	
 	/**
 	 * To delete a user.
-	 * 
 	 * @return String
 	 */
-	@Action(value = "/deleteTeam", results = { @Result(name = "success", location = "/content/listTeam.jsp") })
-	public String delete() {
-		HttpServletRequest request = (HttpServletRequest) ActionContext
-				.getContext().get(ServletActionContext.HTTP_REQUEST);
+	@Action( value   = "/deleteTeam",
+			 results = { @Result(name  = "success", location = "/content/listTeam.jsp") }
+	) 	
+	public String delete()
+	{
+		HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
 		try {
 			teamManager.deleteTeam(Long.parseLong(request.getParameter("id")));
-		} catch (DataIntegrityViolationException die) {
-			System.out.println("data integration exc in action...");
 		}
+		catch(DataIntegrityViolationException die)
+		{
+			System.out.println("data integration exc in action...");
+		}			
 		return search();
 	}
-
+	
 	/**
 	 * To list a single user by Id.
-	 * 
 	 * @return String
 	 */
-	@Action(value = "/editTeam", results = { @Result(name = "editTeam", location = "/content/editTeam.jsp") })
-	public String edit() {
-		HttpServletRequest request = (HttpServletRequest) ActionContext
-				.getContext().get(ServletActionContext.HTTP_REQUEST);
-		team = teamManager.getTeamById(Long.parseLong(request
-				.getParameter("id")));
-		divisionList = teamManager.listDivisions();
+	@Action( value   = "/editTeam",
+			 results = { @Result(name  = "editTeam", location = "/content/editTeam.jsp") }
+	) 		
+	public String edit()
+	{
+		HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
+		team = teamManager.getTeamById(Long.parseLong(request.getParameter("id")));
+		divisionList = divisionManager.listDivisions();
 		return ProjectUtil.EDIT_TEAM;
 	}
-
+	
 	/**
 	 * To list a single user by Id.
-	 * 
 	 * @return String
 	 */
-	@Action(value = "/changeTeam", results = { @Result(name = "changeTeam", location = "/content/changeTeam.jsp") })
-	public String change() {
-		HttpServletRequest request = (HttpServletRequest) ActionContext
-				.getContext().get(ServletActionContext.HTTP_REQUEST);
-
-		player = playerManager.getPlayerById(Long.parseLong(request
-				.getParameter("idPlayer")));
-		teamList = teamManager.listTeams();
-
+	@Action( value   = "/changeTeam",
+			 results = { @Result(name  = "changeTeam", location = "/content/changeTeam.jsp") }
+	) 	
+	public String change()
+	{
+		HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
+		
+		player = playerManager.getPlayerById(Long.parseLong(request.getParameter("idPlayer")));
+		Team team = teamManager.getTeamById(player.getTeamId());
+		nationList = nationManager.listNations();
+		teamList = teamManager.listTeamsByDivision(team.getDivision().getId());
+		divisionList = divisionManager.listDivisionsByNation(team.getNation().getId());
+		
 		return ProjectUtil.CHANGE_TEAM;
-	}
-
-	@Action(value = "/getDynamicImageTeam")
+	}		
+	
+	@Action( value   = "/getDynamicImageTeam") 
 	public void getDynamicImage() {
-		HttpServletRequest request = (HttpServletRequest) ActionContext
-				.getContext().get(ServletActionContext.HTTP_REQUEST);
+
+		HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
 		ServletOutputStream out = null;
+
 		try {
 			HttpServletResponse response = ServletActionContext.getResponse();
 			response.setContentType("multipart/form-data");
 			out = response.getOutputStream();
-			team = teamManager.getTeamById(Long.parseLong(request
-					.getParameter("id")));
+			team = teamManager.getTeamById(Long.parseLong(request.getParameter("id")));
+
 			assert team.getImage() != null : "Immagine non trovata";
+
 			out.write(team.getImage());
 			out.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
+
+		} 
+		finally 
+		{
 			if (out != null)
 				try {
 					out.close();
@@ -244,50 +273,57 @@ public class TeamAction extends FileUploadAction implements
 					e.printStackTrace();
 				}
 		}
-	}
-
-	/*
-	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+	} 
+	
+	/* _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
 	 * 
-	 * METODI GET E SET
+	 *                             METODI GET E SET
 	 * 
 	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
-	 */
-
-	public Team getTeam() {
+	 * */	
+	
+	public Team getTeam() 
+	{
 		return team;
 	}
 
-	public void setTeam(Team team) {
+	public void setTeam(Team team) 
+	{
 		this.team = team;
 	}
-
-	public Player getPlayer() {
+	
+	public Player getPlayer() 
+	{
 		return player;
 	}
 
-	public void setPlayer(Player player) {
+	public void setPlayer(Player player)
+	{
 		this.player = player;
-	}
+	}	
 
-	public List<Team> getTeamList() {
+	public List<Team> getTeamList() 
+	{
 		return teamList;
 	}
-
-	public List<Nation> getNationList() {
+	
+	public List<Nation> getNationList() 
+	{
 		return nationList;
-	}
-
+	}	
+	
 	public List<Player> getPlayerList() {
 		return playerList;
-	}
+	}	
 
-	public List<Division> getDivisionList() {
+	public List<Division> getDivisionList() 
+	{
 		return divisionList;
 	}
-
+	
 	@Override
 	public void setServletRequest(HttpServletRequest servletRequest) {
 		this.servletRequest = servletRequest;
-	}
+	}	
+
 }
