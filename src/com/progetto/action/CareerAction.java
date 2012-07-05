117ef02168ed8cdbbbd9663e4be90d7ab0f9970f
danesiale@gmail.com <danesiale@gmail.com@966f9feb-cae3-6a02-026f-4ad8e5c7e621>
2012-07-05 12:31:35 +0000

    modifica

--- src/com/progetto/action/CareerAction.java
+++ src/com/progetto/action/CareerAction.java
@@ -1,9 +1,16 @@
 package com.progetto.action;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
@@ -13,11 +20,7 @@ import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.interceptor.ServletRequestAware;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
-import com.googlecode.s2hibernate.struts2.plugin.annotations.SessionTarget;
-import com.googlecode.s2hibernate.struts2.plugin.annotations.TransactionTarget;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ModelDriven;
 import com.progetto.FileUploadAction;
@@ -30,28 +33,18 @@ import com.progetto.manager.PlayerManager;
 import com.progetto.manager.TeamManager;
 import com.progetto.util.ProjectUtil;
 
-public class CareerAction extends FileUploadAction implements ServletRequestAware,
-		ModelDriven<Career> {
+public class CareerAction extends FileUploadAction implements ServletRequestAware, ModelDriven<Career> 
+{
 	private static final long serialVersionUID = 1L;
 
-	@SessionTarget
-	Session session;
-
-	@TransactionTarget
-	Transaction transaction;
-
 	private TeamManager teamManager;
-
 	private PlayerManager playerManager;
-
 	private CareerManager careerManager;
 
 	private List<Team> teamList;
-
 	private List<Career> careerList;
 
 	private Career career;
-
 	private Player player;
 
 	private ServletRequest request;
@@ -60,8 +53,10 @@ public class CareerAction extends FileUploadAction implements ServletRequestAwar
 		teamManager = (TeamManager) SpringHelper.getBean("teamManager");
 		playerManager = (PlayerManager) SpringHelper.getBean("playerManager");
 		careerManager = (CareerManager) SpringHelper.getBean("careerManager");
+
 		teamList = new ArrayList<Team>();
 		careerList = new ArrayList<Career>();
+
 		career = new Career();
 	}
 
@@ -83,14 +78,11 @@ public class CareerAction extends FileUploadAction implements ServletRequestAwar
 	 * 
 	 * @return String
 	 */
-	@Action(value = "/insertCareer", 
-			results = { @Result(name = "editCareer", location = "/content/editCareer.jsp") })
+	@Action(value = "/insertCareer", results = { @Result(name = "editCareer", location = "/content/secure/editCareer.jsp") })
 	public String insert() {
 		teamList = teamManager.listTeams();
-		HttpServletRequest request = (HttpServletRequest) ActionContext
-				.getContext().get(ServletActionContext.HTTP_REQUEST);
-		request.setAttribute("playerId", Long.parseLong(request
-				.getParameter("playerId")));
+		HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
+		request.setAttribute("playerId", Long.parseLong(request.getParameter("playerId")));
 
 		return ProjectUtil.EDIT_CAREER;
 	}
@@ -100,10 +92,9 @@ public class CareerAction extends FileUploadAction implements ServletRequestAwar
 	 * 
 	 * @return String
 	 */
-	@Action(value = "/editCareer", results = { @Result(name = "editCareer", location = "/content/editCareer.jsp") })
+	@Action(value = "/editCareer", results = { @Result(name = "editCareer", location = "/content/secure/editCareer.jsp") })
 	public String edit() {
-		career = careerManager.getCareerById(Long.parseLong(request
-				.getParameter("id")));
+		career = careerManager.getCareerById(Long.parseLong(request.getParameter("id")));
 		teamList = teamManager.listTeams();
 
 		return insert();
@@ -116,19 +107,23 @@ public class CareerAction extends FileUploadAction implements ServletRequestAwar
 	 */
 	@Action(value = "/saveCareer", results = {
 			@Result(name = "success", type = "redirect", location = "/viewPlayer.action?id=${player.id}"),
-			@Result(name = "input", location = "/content/editCareer.jsp") }
+			@Result(name = "input", location = "/content/editPlayer.jsp") }
 
 	)
-	public String save() {
+	public String save() 
+	{
 		Team team = teamManager.getTeamById(Long.parseLong(request.getParameter("team.id")));
 		career.setTeam(team);
 		
-		if (userImage != null) {
+		if (userImage != null) 
+		{
 			byte[] image = getFileBytes();
 			career.setImage(image);
-		} else if (career.getId() != null) {
+		} 
+		else if (career.getId() != null) 
+		{
 			career.setImage(careerManager.getCareerById(career.getId()).getImage());
-		}	
+		}
 
 		careerManager.saveOrUpdateCareer(career);
 		return SUCCESS;
@@ -151,25 +146,28 @@ public class CareerAction extends FileUploadAction implements ServletRequestAwar
 
 		return ProjectUtil.VIEW_CAREER;
 	}
-	
+
 	@Action(value = "/getDynamicImageCareer")
 	public void getDynamicImage() {
-		HttpServletRequest request = (HttpServletRequest) ActionContext
-				.getContext().get(ServletActionContext.HTTP_REQUEST);
+
+		HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
 		ServletOutputStream out = null;
+
 		try {
 			HttpServletResponse response = ServletActionContext.getResponse();
 			response.setContentType("multipart/form-data");
 			out = response.getOutputStream();
 			career = careerManager.getCareerById(Long.parseLong(request.getParameter("id")));
+
 			assert career.getImage() != null : "Immagine non trovata";
-			out.write(career.getImage());
+
+			out.write(getResizeImage(career.getImage()));
 			out.flush();
-		} catch (IOException e) {
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
+		} 
+		finally {
 			if (out != null)
 				try {
 					out.close();
@@ -177,14 +175,35 @@ public class CareerAction extends FileUploadAction implements ServletRequestAwar
 					e.printStackTrace();
 				}
 		}
+	}
+	
+	private byte[] getResizeImage(byte[] image) throws IOException
+	{		
+		BufferedImage bsrc = ImageIO.read(new ByteArrayInputStream(image));
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		BufferedImage bdest = new BufferedImage(MIN_WIDTH, MIN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = bdest.createGraphics();
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+		
+		double scalex = (double) MIN_WIDTH / bsrc.getWidth();
+		double scaley = (double) MIN_HEIGHT / bsrc.getHeight();
+		
+		AffineTransform at = AffineTransform.getScaleInstance(scalex, scaley);
+		g.drawRenderedImage(bsrc, at);
+		g.dispose();
+		
+		ImageIO.write(bdest, "JPG", baos);
+		
+		return baos.toByteArray();
 	}	
 
 	/*
-	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_
 	 * 
 	 * METODI GET E SET
 	 * 
-	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+	 * =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
 	 */
 
 	public Career getCareer() {
