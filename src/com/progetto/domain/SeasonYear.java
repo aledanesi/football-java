9018f9914848900df52421164aef00be5c030aea
danesiale@gmail.com <danesiale@gmail.com@966f9feb-cae3-6a02-026f-4ad8e5c7e621>
2012-07-16 13:39:21 +0000

    modifica

---
+++ src/com/progetto/domain/SeasonYear.java
@@ -0,0 +1,79 @@
+/*
+ * Progetto, a new java application 
+ *
+ * Copyright (c) 2010, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Rattazzi Street, Fifth Floor
+ * Pomezia, RM  00040  Italy
+ */
+package com.progetto.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Season class.
+ * The class describe the season.
+ *
+ * @author Alessandro Danesi
+ */
+@Entity
+@Table(name="SEASON_YEAR")
+public class SeasonYear implements Serializable 
+{
+	// serial version UID
+	private static final long serialVersionUID = 7526472295622776147L;
+	
+	/*************************************************
+	  
+	  FIELDS
+	  
+	 **************************************************/	
+
+	@Id
+	@GeneratedValue
+	@Column(name="ID")	
+	private Long id;
+	
+	@Column(name="VALUE")	
+	private String value;
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}
