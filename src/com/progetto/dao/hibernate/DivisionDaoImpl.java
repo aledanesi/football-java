bfd7ffb50bf1b8489bb46abb96cddc8a77ff290f
danesiale@gmail.com <danesiale@gmail.com@966f9feb-cae3-6a02-026f-4ad8e5c7e621>
2012-07-18 15:27:02 +0000

    modifica

--- src/com/progetto/dao/hibernate/DivisionDaoImpl.java
+++ src/com/progetto/dao/hibernate/DivisionDaoImpl.java
@@ -25,32 +25,54 @@ package com.progetto.dao.hibernate;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import com.progetto.dao.DivisionDao;
 import com.progetto.domain.Division;
+import com.progetto.domain.Season;
 
 /**
  * @author C_ICTDNS
- * 
+ *
  */
 @Repository("divisionDAO")
-public class DivisionDaoImpl extends HibernateDaoSupport implements DivisionDao {
+public class DivisionDaoImpl extends HibernateDaoSupport implements DivisionDao
+{
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public List<Division> listDivisions() {
-		return getHibernateTemplate().find("from Division d order by d.id");
+		return getHibernateTemplate().find("from Division d order by d.id");	
 	}
+	
+	@Override
+	public Division getDivision(Long teamId, Long seasonYearId) {
+		Session session = getHibernateTemplate().getSessionFactory().getCurrentSession();
+		Criteria criteria = session.createCriteria(Season.class);
+		criteria.setFetchMode("Division", FetchMode.JOIN);
+		criteria.add(Restrictions.eq("team.id", teamId));
+		criteria.add(Restrictions.eq("seasonYear.id", seasonYearId));
+		
+		Season season = (Season)criteria.uniqueResult();
+		
+		Division division = null;
+		if (season != null)
+		{
+			division = season.getDivision();	
+		}		
+		return division;		
+	}	
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public List<Division> listDivisionsByNation(Long nationId) {
 		// TODO Auto-generated method stub
-		return getHibernateTemplate()
-				.find("from Division d where nation_id = ? order by d.level",
-						nationId);
+		return getHibernateTemplate().find("from Division d where nation_id = ? order by d.level", nationId);
 	}
 
 }
