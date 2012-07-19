2411ea1d636fb6e7253011c7abf6dd4a795c6d44
danesiale@gmail.com <danesiale@gmail.com@966f9feb-cae3-6a02-026f-4ad8e5c7e621>
2012-07-19 09:09:10 +0000

    modifica

--- src/com/progetto/action/CareerAction.java
+++ src/com/progetto/action/CareerAction.java
@@ -27,203 +27,229 @@ import com.progetto.FileUploadAction;
 import com.progetto.SpringHelper;
 import com.progetto.domain.Career;
 import com.progetto.domain.Player;
+import com.progetto.domain.SeasonYear;
 import com.progetto.domain.Team;
 import com.progetto.manager.CareerManager;
 import com.progetto.manager.PlayerManager;
+import com.progetto.manager.SeasonManager;
 import com.progetto.manager.TeamManager;
 import com.progetto.util.ProjectUtil;
 
 public class CareerAction extends FileUploadAction implements ServletRequestAware, ModelDriven<Career> 
 {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  private TeamManager teamManager;
-  private PlayerManager playerManager;
-  private CareerManager careerManager;
+	private TeamManager teamManager;
+	private PlayerManager playerManager;
+	private CareerManager careerManager;
+	private SeasonManager seasonManager;
 
-  private List<Team> teamList;
-  private List<Career> careerList;
+	private List<Team> teamList;
+	private List<Career> careerList;
+	private List<SeasonYear> seasonYearList;
 
-  private Career career;
-  private Player player;
+	private Career career;
+	private Player player;
 
-  private ServletRequest request;
+	private ServletRequest request;
 
-  public CareerAction() {
-    teamManager = (TeamManager) SpringHelper.getBean("teamManager");
-    playerManager = (PlayerManager) SpringHelper.getBean("playerManager");
-    careerManager = (CareerManager) SpringHelper.getBean("careerManager");
+	public CareerAction() {
+		teamManager = (TeamManager) SpringHelper.getBean("teamManager");
+		playerManager = (PlayerManager) SpringHelper.getBean("playerManager");
+		careerManager = (CareerManager) SpringHelper.getBean("careerManager");
+		seasonManager = (SeasonManager) SpringHelper.getBean("seasonManager");
 
-    teamList = new ArrayList<Team>();
-    careerList = new ArrayList<Career>();
+		teamList = new ArrayList<Team>();
+		seasonYearList = new ArrayList<SeasonYear>();
+		careerList = new ArrayList<Career>();
 
-    career = new Career();
-  }
+		career = new Career();
+	}
 
-  @Override
-  public Career getModel() {
-    return career;
-  }
+	@Override
+	public Career getModel() {
+		return career;
+	}
 
-  /*
-   * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
-   * 
-   * METODI DI ACTION
-   * 
-   * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
-   */
+	/*
+	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+	 * 
+	 * METODI DI ACTION
+	 * 
+	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+	 */
 
-  /**
-   * To list a single user by Id.
-   * 
-   * @return String
-   */
-  @Action(value = "/insertCareer", results = { @Result(name = "editCareer", location = "/content/secure/editCareer.jsp") })
-  public String insert() {
-    teamList = teamManager.listTeams();
-    HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
-    request.setAttribute("playerId", Long.parseLong(request.getParameter("playerId")));
+	/**
+	 * To list a single user by Id.
+	 * 
+	 * @return String
+	 */
+	@Action(value = "/insertCareer", results = { @Result(name = "editCareer", location = "/content/secure/editCareer.jsp") })
+	public String insert() {
+		teamList = teamManager.listTeams();
+		seasonYearList = seasonManager.listYears();
+		HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
+		request.setAttribute("playerId", Long.parseLong(request.getParameter("playerId")));
 
-    return ProjectUtil.EDIT_CAREER;
-  }
+		return ProjectUtil.EDIT_CAREER;
+	}
 
-  /**
-   * To list a single user by Id.
-   * 
-   * @return String
-   */
-  @Action(value = "/editCareer", results = { @Result(name = "editCareer", location = "/content/secure/editCareer.jsp") })
-  public String edit() {
-    career = careerManager.getCareerById(Long.parseLong(request.getParameter("id")));
-    teamList = teamManager.listTeams();
+	/**
+	 * To list a single user by Id.
+	 * 
+	 * @return String
+	 */
+	@Action(value = "/editCareer", results = { @Result(name = "editCareer", location = "/content/secure/editCareer.jsp") })
+	public String edit() {
+		career = careerManager.getCareerById(Long.parseLong(request.getParameter("id")));
+		teamList = teamManager.listTeams();
+		seasonYearList = seasonManager.listYears();
 
-    return insert();
-  }
+		return insert();
+	}
 
-  /**
-   * To save or update career.
-   * 
-   * @return String
-   */
-  @Action(value = "/saveCareer", results = {
-      @Result(name = "success", type = "redirect", location = "/viewPlayer.action?id=${player.id}"),
-      @Result(name = "input", location = "/content/editPlayer.jsp") }
+	/**
+	 * To save or update career.
+	 * 
+	 * @return String
+	 */
+	@Action(value = "/saveCareer", results = {
+			@Result(name = "success", type = "redirect", location = "/viewPlayer.action?id=${player.id}"),
+			@Result(name = "input", location = "/content/editPlayer.jsp") }
 
-  )
-  public String save() 
-  {
-    Team team = teamManager.getTeamById(Long.parseLong(request.getParameter("team.id")));
-    career.setTeam(team);
-    
-    if (userImage != null) 
-    {
-      byte[] image = getFileBytes();
-      career.setImage(image);
-    } 
-    else if (career.getId() != null) 
-    {
-      career.setImage(careerManager.getCareerById(career.getId()).getImage());
-    }
+	)
+	public String save() 
+	{	
+		Team team = teamManager.getTeamById(Long.parseLong(request.getParameter("team.id")));
+		career.setTeam(team);
+		
+		if (userImage != null) 
+		{
+			byte[] image = getFileBytes();
+			career.setImage(image);
+		} 
+		else if (career.getId() != null) 
+		{
+			Career careerImage = careerManager.getCareerById(career.getId());
+			if (careerImage != null)
+			{
+				career.setImage(careerImage.getImage());	
+			}
+		}
 
-    careerManager.saveOrUpdateCareer(career);
-    return SUCCESS;
-  }
+		
+		try 
+		{
+			careerManager.saveOrUpdateCareer(career);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}	
 
-  /**
-   * To list a single user by Id.
-   * 
-   * @return String
-   */
-  @Action(value = "/viewCareer", results = { @Result(name = "viewCareer", location = "/content/viewCareer.jsp") })
-  public String view() {
-    Long idPlayer = Long.parseLong(request.getParameter("playerId"));
-    Long idCareer = Long.parseLong(request.getParameter("id"));
-    player = playerManager.getPlayerById(idPlayer);
-    career = careerManager.getCareerById(idCareer);
+		
+		return SUCCESS;
+	}
 
-    request.setAttribute("player", player);
-    request.setAttribute("career", career);
+	/**
+	 * To list a single user by Id.
+	 * 
+	 * @return String
+	 */
+	@Action(value = "/viewCareer", results = { @Result(name = "viewCareer", location = "/content/viewCareer.jsp") })
+	public String view() {
+		Long idPlayer = Long.parseLong(request.getParameter("playerId"));
+		Long idCareer = Long.parseLong(request.getParameter("id"));
+		player = playerManager.getPlayerById(idPlayer);
+		career = careerManager.getCareerById(idCareer);
 
-    return ProjectUtil.VIEW_CAREER;
-  }
+		request.setAttribute("player", player);
+		request.setAttribute("career", career);
 
-  @Action(value = "/getDynamicImageCareer")
-  public void getDynamicImage() {
+		return ProjectUtil.VIEW_CAREER;
+	}
 
-    HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
-    ServletOutputStream out = null;
+	@Action(value = "/getDynamicImageCareer")
+	public void getDynamicImage() {
 
-    try {
-      HttpServletResponse response = ServletActionContext.getResponse();
-      response.setContentType("multipart/form-data");
-      out = response.getOutputStream();
-      career = careerManager.getCareerById(Long.parseLong(request.getParameter("id")));
+		HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
+		ServletOutputStream out = null;
 
-      assert career.getImage() != null : "Immagine non trovata";
+		try {
+			HttpServletResponse response = ServletActionContext.getResponse();
+			response.setContentType("multipart/form-data");
+			out = response.getOutputStream();
+			career = careerManager.getCareerById(Long.parseLong(request.getParameter("id")));
 
-      out.write(getResizeImage(career.getImage()));
-      out.flush();
-    } 
-    catch (Exception e) {
-      e.printStackTrace();
-    } 
-    finally {
-      if (out != null)
-        try {
-          out.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-    }
-  }
-  
-  private byte[] getResizeImage(byte[] image) throws IOException
-  {    
-    BufferedImage bsrc = ImageIO.read(new ByteArrayInputStream(image));
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    
-    BufferedImage bdest = new BufferedImage(MIN_WIDTH, MIN_HEIGHT, BufferedImage.TYPE_INT_RGB);
-    Graphics2D g = bdest.createGraphics();
-    g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-    
-    double scalex = (double) MIN_WIDTH / bsrc.getWidth();
-    double scaley = (double) MIN_HEIGHT / bsrc.getHeight();
-    
-    AffineTransform at = AffineTransform.getScaleInstance(scalex, scaley);
-    g.drawRenderedImage(bsrc, at);
-    g.dispose();
-    
-    ImageIO.write(bdest, "JPG", baos);
-    
-    return baos.toByteArray();
-  }  
+			assert career.getImage() != null : "Immagine non trovata";
 
-  /*
-   * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_
-   * 
-   * METODI GET E SET
-   * 
-   * =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
-   */
+			out.write(getResizeImage(career.getImage()));
+			out.flush();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		} 
+		finally {
+			if (out != null)
+				try {
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+	}
+	
+	private byte[] getResizeImage(byte[] image) throws IOException
+	{		
+		BufferedImage bsrc = ImageIO.read(new ByteArrayInputStream(image));
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		BufferedImage bdest = new BufferedImage(MIN_WIDTH, MIN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = bdest.createGraphics();
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+		
+		double scalex = (double) MIN_WIDTH / bsrc.getWidth();
+		double scaley = (double) MIN_HEIGHT / bsrc.getHeight();
+		
+		AffineTransform at = AffineTransform.getScaleInstance(scalex, scaley);
+		g.drawRenderedImage(bsrc, at);
+		g.dispose();
+		
+		ImageIO.write(bdest, "JPG", baos);
+		
+		return baos.toByteArray();
+	}	
 
-  public Career getCareer() {
-    return career;
-  }
+	/*
+	 * _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_
+	 * 
+	 * METODI GET E SET
+	 * 
+	 * =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+	 */
 
-  public void setCareer(Career career) {
-    this.career = career;
-  }
+	public Career getCareer() {
+		return career;
+	}
 
-  public List<Team> getTeamList() {
-    return teamList;
-  }
+	public void setCareer(Career career) {
+		this.career = career;
+	}
 
-  public List<Career> getCareerList() {
-    return careerList;
-  }
+	public List<Team> getTeamList() {
+		return teamList;
+	}
 
-  @Override
-  public void setServletRequest(HttpServletRequest servletRequest) {
-    this.request = servletRequest;
-  }
+	public List<Career> getCareerList() {
+		return careerList;
+	}
+	
+	public List<SeasonYear> getSeasonYearList() {
+		return seasonYearList;
+	}	
+
+	@Override
+	public void setServletRequest(HttpServletRequest servletRequest) {
+		this.request = servletRequest;
+	}
 }
